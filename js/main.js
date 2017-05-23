ee98787cc3eafcdcb9db20783cac4e3a69d76a3c
Tony Wells <tony020@gmail.com>
2017-05-23 14:13:17 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log ("up and runing");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards [0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards [2];
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {alert ("you found a match")} 
+		else {alert ("sorry, try again")}
+};
