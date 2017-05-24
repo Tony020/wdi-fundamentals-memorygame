ed295005a764fa10da0cc9b7d8cc5daba337b586
Tony Wells <tony020@gmail.com>
2017-05-24 11:19:06 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-console.log ("up and runing");
+// console.log ("up and runing");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards [0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards [2];
-cardsInPlay.push(cardTwo);
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {alert ("you found a match")} 
-		else {alert ("sorry, try again")}
+
+var checkForMatch = function()
+{if (cardsInPlay[0] === cardsInPlay[1]) {
+      console.log("You found a match!");
+  } else {
+      console.log("Sorry, try again.");
+  }
 };
+  
+var flipCard = function(cardID){
+	console.log ("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	// var cardOne = cards [0];
+	// cardsInPlay.push(cardOne);
+	// var cardTwo = cards [2];
+	// cardsInPlay.push(cardTwo);
+	
+};
+flipCard(0);
+flipCard(2);
+checkForMatch();
