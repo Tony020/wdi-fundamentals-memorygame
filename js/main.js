539a25f102315d9223bdf2f2dd576eb6c7cc6a37
Tony Wells <tony020@gmail.com>
2017-05-26 12:52:33 +0100

    Created game logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,42 @@
 // console.log ("up and runing");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	image: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	image: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	image: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	image: "images/king-of-diamonds.png",
+}
+
+];
 var cardsInPlay = [];
 
 var checkForMatch = function()
 {if (cardsInPlay[0] === cardsInPlay[1]) {
-      console.log("You found a match!");
+      alert("You found a match!");
   } else {
-      console.log("Sorry, try again.");
+      alert("Sorry, try again.");
   }
 };
   
 var flipCard = function(cardID){
-	console.log ("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log ("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].rank);
+	console.log(cards[cardID].image);
 	// var cardOne = cards [0];
 	// cardsInPlay.push(cardOne);
 	// var cardTwo = cards [2];
